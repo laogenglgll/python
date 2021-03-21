d2fdfd8378466ec09ec1b6a8507ad48b50d59fdd
laogenglgll <laogeng@outlook.com>
2021-03-21 12:59:41 +0800

    第一次

---
+++ 1.py
@@ -0,0 +1,17 @@
+#0~100的奇数
+
+print("奇数有：")
+
+
+def num():
+    number = 0
+    while number<=100:
+        if number % 2 !=0:
+            print(number)
+        number+=1
+num()
+
+for i in range(100):
+    if i%2==0:
+        print(i)
+#GitHub测试
